59903b11712ded469dcbd660879ba54193b4ad0b
AkRuwais <akruwais9445@gmail.com>
2023-04-07 15:52:59 +0530

    edit 3

--- src/Controller/AdminControllers.js
+++ src/Controller/AdminControllers.js
@@ -1,4 +1,5 @@
 const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
 
 module.exports = {
   getlogin: (req, res) => {
@@ -10,29 +11,35 @@ module.exports = {
   },
   postlogin: async (req, res) => {
     try {
+      const hashedpass = await bcrypt.hash("classroom@2023", 10);
       // console.log(JSON.stringify(req.body));
       const admin = {
         name: "ADMIN",
         email: "adminone@gmail.com",
-        password: "classroom@2023",
+        password: hashedpass,
       };
       console.log(JSON.stringify(admin));
-      if (JSON.stringify(admin) === JSON.stringify(req.body)) {
-        res.send("home");
+      if (admin.name === req.body.name) {
+        bcrypt.compare(req.body.password, admin.password).then((result) => {
+          if (result) {
+            res.send("home");
+          } else {
+            res.send("error password");
+          }
+        });
       } else {
-        res.send("!!Errro!!  please check onetime...");
+        res.send("!!Errro!!  please check username...");
       }
     } catch (error) {
       console.log("error");
     }
   },
 
-  gettutor:(req,res)=>{
+  gettutor: (req, res) => {
     try {
-      res.send("admin can add tutor in here")
+      res.send("admin can add tutor in here");
     } catch (error) {
-      console.log(error
-        );
+      console.log(error);
     }
   },
   posttutor: async (req, res) => {
@@ -45,7 +52,7 @@ module.exports = {
       console.log(error);
     }
   },
-  puttutor:async(req,res)=>{
+  puttutor: async (req, res) => {
     try {
       const signup = await register.insertMany(
         ({ Name, Username, Phone, Email, password } = req.body)
@@ -55,20 +62,18 @@ module.exports = {
       console.log(error);
     }
   },
-  deletetutor:async(req,res)=>{
+  deletetutor: async (req, res) => {
     try {
-    const id = req.params.id
-    await studentsmodel.deleteOne({_id:id}).then(result=>{
-    console.log(result);
-    res.redirect('/students')
-    })
+      const id = req.params.id;
+      await studentsmodel.deleteOne({ _id: id }).then((result) => {
+        console.log(result);
+        res.redirect("/students");
+      });
     } catch (error) {
-    console.log(error);
+      console.log(error);
     }
-    
-},
+  },
 
-
-// student:{
-//   get:(req,res)=>{}
-}
+  // student:{
+  //   get:(req,res)=>{}
+};
